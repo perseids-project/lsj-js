b4d50e38894df98ceadbc92bec3cd00af3813113
zfletch <zfletch2@gmail.com>
2019-01-14 12:36:45 -0500

    Updates

---
+++ precache-manifest.e7a7165b3c685b9986104ecea8204643.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe99705529844fe8d096",
+    "url": "/lsj/static/js/runtime~main.fe997055.js"
+  },
+  {
+    "revision": "f07ac9b8f5efa4dc4207",
+    "url": "/lsj/static/js/main.f07ac9b8.chunk.js"
+  },
+  {
+    "revision": "59fdc46cd17803cdcba5",
+    "url": "/lsj/static/js/2.59fdc46c.chunk.js"
+  },
+  {
+    "revision": "8536ce698e8c30b37b13",
+    "url": "/lsj/static/js/1.8536ce69.chunk.js"
+  },
+  {
+    "revision": "f07ac9b8f5efa4dc4207",
+    "url": "/lsj/static/css/main.75322932.chunk.css"
+  },
+  {
+    "revision": "59fdc46cd17803cdcba5",
+    "url": "/lsj/static/css/2.b6bc2dcc.chunk.css"
+  },
+  {
+    "revision": "c60f5874485e50188fa3ea7175ab9204",
+    "url": "/lsj/index.html"
+  }
+];
