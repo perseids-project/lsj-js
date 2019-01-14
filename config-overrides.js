cabb68fc26b168ec6c98287f9deb13ca48672e13
zfletch <zfletch2@gmail.com>
2019-01-14 12:32:24 -0500

    uneject application
    * uneject the application
    * use react-app-rewired to modify the configuration where necessary
    * create 404.html on build so that React Router works correctly with
      GitHub pages the first time the page is loaded
    * add deploy command to deploy to both GitHub and Perseids

---
+++ config-overrides.js
@@ -0,0 +1,24 @@
+module.exports = {
+  jest(config) {
+    // eslint-disable-next-line no-param-reassign
+    config.moduleNameMapper = {
+      '^react-native$': 'react-native-web',
+      localforage: '<rootDir>/mocks/localforageMock.js',
+      '../dictionaries/dictionary.json': '<rootDir>/mocks/dictionary.json',
+      '../dictionaries/exact-match.json': '<rootDir>/mocks/exact-match.json',
+      '../dictionaries/greek-match.json': '<rootDir>/mocks/greek-match.json',
+      '../dictionaries/latin-match.json': '<rootDir>/mocks/latin-match.json',
+    };
+
+    return config;
+  },
+  webpack(config) {
+    // This is very fragile and liable to change with updates to React Scripts
+    // It is necessary in order to keep build sizes down since there is a lot of Unicode
+    // See https://github.com/facebook/create-react-app/issues/2706
+    // eslint-disable-next-line no-param-reassign
+    config.optimization.minimizer[0].options.terserOptions.output.ascii_only = false;
+
+    return config;
+  },
+};
