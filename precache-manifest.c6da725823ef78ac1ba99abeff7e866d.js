69e7f4065998bdff1eb9781fb851627075d7af37
zfletch <zfletch2@gmail.com>
2019-07-11 14:24:28 -0400

    Updates

---
+++ precache-manifest.c6da725823ef78ac1ba99abeff7e866d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be43ffaee60892fcb5b2848eca211908",
+    "url": "/lsj-js/index.html"
+  },
+  {
+    "revision": "c03dc60bab5b8bbf3734",
+    "url": "/lsj-js/static/css/2.bc649c47.chunk.css"
+  },
+  {
+    "revision": "3dc76105c68e03da1921",
+    "url": "/lsj-js/static/css/main.fc35351d.chunk.css"
+  },
+  {
+    "revision": "c03dc60bab5b8bbf3734",
+    "url": "/lsj-js/static/js/2.289b6dae.chunk.js"
+  },
+  {
+    "revision": "cdaabe0863ad6e192d88",
+    "url": "/lsj-js/static/js/3.752c0250.chunk.js"
+  },
+  {
+    "revision": "3dc76105c68e03da1921",
+    "url": "/lsj-js/static/js/main.3edbd3a0.chunk.js"
+  },
+  {
+    "revision": "8ba44fde92f7def43eba",
+    "url": "/lsj-js/static/js/runtime~main.5f05b0ba.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lsj-js/precache-manifest.659411b39b07e5b0117a73304311f34e.js"
+  "/lsj-js/precache-manifest.c6da725823ef78ac1ba99abeff7e866d.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/lsj-js/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lsj-js/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
